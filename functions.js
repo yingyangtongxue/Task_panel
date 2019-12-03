d440cb62f13d08b5a2d22bebc7a6b8658bfdeed6
yingyangtongxue <55842031+yingyangtongxue@users.noreply.github.com>
2019-12-03 14:44:34 -0300

    Fourth version
    - New features :
    *bar 
    *boxes
    *alert

--- functions.js
+++ functions.js
@@ -4,21 +4,23 @@ let new_task = document.querySelector("#button1");
 new_task.addEventListener("click", function(event){
 	console.log("hello");
 	console.log(count);
-	console.log(count_box);
+	console.log(count_box1);
+
 
 });									
 
 $("#button1").click(function(){
 	$("#add_content").show();
+	 $("#add_content").keypress(function(e){
+	 	if(e.which == 13 || e.keyCode == 13){
+			alert('A tarefa foi adiciona');
+	}
+  });
 }); //show box (button1 = new task)
 	
 $(document).ready(function(){
-  $(document).keypress(function(e){
-	if(e.wich == 13 || e.keyCode == 13){
-		alert('A tecla ENTER foi pressionada');
-	}
-  })
-});	
+
+});	//(estatica)
 
 /*
 $(document).ready(function(){
@@ -31,7 +33,8 @@ $( document ).on( 'keydown', function ( e ) {
     if ( e.keyCode === 27 ) { // ESC
         $("#add_content").hide();
     }
-}); //close box 
+}); //close box (dinamica)
+
 
 function countId(id) {
    var counter = 0;
@@ -42,37 +45,50 @@ function countId(id) {
    });
 }
 
-var count = $('.box').length;
-var count_box1 = $('#box_1').length;
-var count_box2 = $('#box_2').length; //utilizar esse cálculo p as barras coloridas (numero de elementos na caixa/ qntdd total de elementos)
-var count_box3 = $('#box_3').length;
-var count_box4 = $('#box_4').length;
-
+var count = $('p').length;
+var count_box1 = $('.p1').length;
+var count_box2 = $('.p2').length; //utilizar esse cálculo p as barras coloridas (numero de elementos na caixa/ qntdd total de elementos)
+var count_box3 = $('.p3').length;
+var count_box4 = $('.p4').length;
+function atualizar(){
+	count = $('p').length;
+	count_box1 = $('.p1').length;
+	count_box2 = $('.p2').length; //utilizar esse cálculo p as barras coloridas (numero de elementos na caixa/ qntdd total de elementos)
+	count_box3 = $('.p3').length;
+	count_box4 = $('.p4').length;
+	modificar_tamanho();
+}
 
 $(document).ready(function(){
   $(".p1").click(function(){
   	//$(".p1").appendTo(".p2");
-    $(this).remove();
-    //$('.p1').removeClass('p1').addClass('p2');
-    $(".p2").after(this);
+   	 $(this).remove();
+     $(this).removeClass('p1').addClass('p2');
+   	 $("#box_2").append(this);
+   	 	atualizar();
   });
-  //$('.p1').removeClass('p1').addClass('p2');
-}); //move os elementos da p1 p p2
-
-
-$(document).ready(function(){
-	//if(isEmpty(''))
   $(".p2").click(function(){
+  	//$(".p1").appendTo(".p2");
     $(this).remove();
-    $(".p3").after(this);
     //$('.p1').removeClass('p1').addClass('p2');
+     $(this).removeClass('p2').addClass('p3');
+   	 $("#box_3").append(this);
+   	 	atualizar();
   });
-}); //move p2 p p3
-
-$(document).ready(function(){
   $(".p3").click(function(){
     $(this).remove();
-    $(".p4").after(this);
+     $(this).removeClass('p3').addClass('p4');
+   	 $("#box_4").append(this);
+   	 	atualizar();
     //$('.p3').removeClass('p3').addClass('p4');
   });
-}); // move p3 p p4
+});
+
+function modificar_tamanho(){
+	$("#green").css("width",(count_box1/count*100)+"%");
+	$("#red").css("width",(count_box2/count*100)+"%");
+	$("#purple").css("width",(count_box3/count*100)+"%");
+	$("#light_blue").css("width",(count_box4/count*100)+"%");
+}
+$(document).ready(modificar_tamanho); //barra colorida movimentação
+
